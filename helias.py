3f3d967ab4e34f3aa636c65d2a601e492864d114
Lumastor <lumino110908@gmail.com>
2026-04-09 10:32:36 +0200

    Update helias.py

--- helias.py
+++ helias.py
@@ -16,20 +16,43 @@ V_CENTER = 90
 h_angle = H_CENTER
 v_angle = V_CENTER
 
-def set_leg(h, v):
+def set_leg0(h, v):
     pwm.servo_angle(0, h)
     pwm.servo_angle(1, v)
+    
+def set_leg1(h, v):
+    pwm.servo_angle(2, h)
+    pwm.servo_angle(3, v)
+    
+def set_leg2(h, v):
+    pwm.servo_angle(14, h)
+    pwm.servo_angle(15, v)
+
+def set_leg3(h, v):
+    pwm.servo_angle(12, h)
+    pwm.servo_angle(13, v)
+    
+
 
 # ================= POSITIONS =================
 
 def neutral():
-    set_leg(H_CENTER, V_CENTER)
+    set_leg0(H_CENTER, V_CENTER)
+    set_leg1(H_CENTER, V_CENTER)
+    set_leg2(H_CENTER, V_CENTER)
+    set_leg3(H_CENTER, V_CENTER)
 
 def lift():
-    set_leg(h_angle, 60)   # lever patte
+    set_leg0(h_angle, 60)   # lever patte
+    set_leg1(h_angle, 60)   # lever patte
+    set_leg2(h_angle, 60)   # lever patte
+    set_leg3(h_angle, 60)   # lever patte
 
 def down():
-    set_leg(h_angle, 120)  # poser patte
+    set_leg0(h_angle, 120)  # poser patte
+    set_leg1(h_angle, 120)  # poser patte
+    set_leg2(h_angle, 120)  # poser patte
+    set_leg3(h_angle, 120)  # poser patte
 
 # ================= MOUVEMENTS =================
 
@@ -41,14 +64,20 @@ def forward():
     time.sleep(0.1)
 
     h_angle = 120
-    set_leg(h_angle, 60)
+    set_leg0(h_angle, 60)
+    set_leg1(h_angle, 60)
+    set_leg2(h_angle, 60)
+    set_leg3(h_angle, 60)
     time.sleep(0.1)
 
     down()
     time.sleep(0.1)
 
     h_angle = 60
-    set_leg(h_angle, 120)
+    set_leg0(h_angle, 120)
+    set_leg1(h_angle, 120)
+    set_leg2(h_angle, 120)
+    set_leg3(h_angle, 120)
     time.sleep(0.1)
 
 
@@ -59,14 +88,20 @@ def backward():
     time.sleep(0.1)
 
     h_angle = 60
-    set_leg(h_angle, 60)
+    set_leg0(h_angle, 60)
+    set_leg1(h_angle, 60)
+    set_leg2(h_angle, 60)
+    set_leg3(h_angle, 60)
     time.sleep(0.1)
 
     down()
     time.sleep(0.1)
 
     h_angle = 120
-    set_leg(h_angle, 120)
+    set_leg0(h_angle, 120)
+    set_leg1(h_angle, 120)
+    set_leg2(h_angle, 120)
+    set_leg3(h_angle, 120)
     time.sleep(0.1)
 
 
@@ -74,7 +109,10 @@ def turn_left():
     lift()
     time.sleep(0.1)
 
-    set_leg(60, 60)
+    set_leg0(60, 60)
+    set_leg1(60, 60)
+    set_leg2(60, 60)
+    set_leg3(60, 60)
     time.sleep(0.2)
 
     down()
@@ -84,7 +122,10 @@ def turn_right():
     lift()
     time.sleep(0.1)
 
-    set_leg(120, 60)
+    set_leg0(120, 60)
+    set_leg1(120, 60)
+    set_leg2(120, 60)
+    set_leg3(120, 60)
     time.sleep(0.2)
 
     down()
@@ -92,7 +133,13 @@ def turn_right():
 
 def attack():
     for _ in range(3):
-        set_leg(90, 50)
-        time.sleep(0.1)
-        set_leg(90, 130)
-        time.sleep(0.1)
+        set_leg0(90, 50)
+        set_leg1(90, 50)
+        set_leg2(90, 50)
+        set_leg3(90, 50)
+        time.sleep(0.15)
+        set_leg0(90, 130)
+        set_leg1(90, 130)
+        set_leg2(90, 130)
+        set_leg3(90, 130)
+        time.sleep(0.15)
